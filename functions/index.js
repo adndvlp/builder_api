99950423708380a64194d40c1d5cfc56e0a50d6a
adndvlp <andngdv.lpr@gmail.com>
2026-07-30 22:53:41 -0600

    refactor with tests

--- functions/index.js
+++ functions/index.js
@@ -3,15 +3,17 @@ import {
   apiDataComplete,
   finalizeDisconnectedSessions,
 } from "./experiment/sessions/index.js";
+import { processSessionTimeout } from "./experiment/sessions/timeout/tasks.js";
 import { publishExperiment } from "./experiment/index.js";
 import { apiDeleteExperiment } from "./experiment/index.js";
-import { apiCondition } from "./experiment/api-condition.js";
-import { dropboxOAuthCallback } from "./oauth/callbacks/dropbox.js";
-import { githubOAuthCallback } from "./oauth/callbacks/github.js";
-import { googleDriveOAuthCallback } from "./oauth/callbacks/google-drive.js";
-import { osfOAuthCallback } from "./oauth/callbacks/osf.js";
-import { osfManage } from "./oauth/osf-token.js";
-import { uploadParticipantFile } from "./experiment/participant-files.js";
+import { apiCondition } from "./experiment/api/condition.js";
+import { dropboxOAuthCallback } from "./oauth/api/callbacks/dropbox.js";
+import { githubOAuthCallback } from "./oauth/api/callbacks/github.js";
+import { googleDriveOAuthCallback } from "./oauth/api/callbacks/google-drive.js";
+import { osfOAuthCallback } from "./oauth/api/callbacks/osf.js";
+import { osfManage } from "./oauth/api/osf-manage.js";
+import { createOAuthStateEndpoint } from "./oauth/api/state.js";
+import { uploadParticipantFile } from "./experiment/participant-files/api/upload.js";
 import { setGlobalOptions } from "firebase-functions/v2";
 
 setGlobalOptions({
@@ -24,11 +26,13 @@ export {
   apiDeleteExperiment,
   apiCondition,
   finalizeDisconnectedSessions,
+  processSessionTimeout,
   dropboxOAuthCallback,
   githubOAuthCallback,
   publishExperiment,
   googleDriveOAuthCallback,
   osfManage,
   osfOAuthCallback,
+  createOAuthStateEndpoint,
   uploadParticipantFile,
 };
